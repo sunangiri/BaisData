e852acff836b2a43deef1cc8bacc8f5d4abcb2fa
M Khoirul Risqi <82764602+risqikhoirul@users.noreply.github.com>
2023-02-22 11:23:31 +0700

    Delete Tugas1.java

--- Tugas1.java
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE fst;
-USE fst;
-CREATE TABLE mahasiswa (nama varchar(50) not null, alamat varchar(50) not null, email varchar(50) not null);
-
-SHOW DATABASES;
-SHOW TABLES;
-DESC mahasiswa;
-
-INSERT INTO mahasiswa ("M Khoirul Risqi", "Desa Ngumpak Dalem", "risqisgb@gmail.com");
